a2529686a14fdb10677828973eda593d504e4764
MMD <m.yazdani000@gmail.com>
2020-03-30 17:21:36 +0430

    insert news to chanell

--- rss_bot.py
+++ rss_bot.py
@@ -1,13 +1,16 @@
+import datetime
 import feedparser
 import time
 from pymongo import MongoClient
-import datetime
+from pyrogram import Client, MessageHandler
+
 # ---------------------------------------- >> Connect to Mongodb
 client = MongoClient('localhost', 27017)
 db = client.test_database
 collection = db.mmd
 # ---------------------------------------- >> Connect to rss site
 feed = feedparser.parse("https://www.yjc.ir/fa/rss/3")
+app = Client('my_bot')
 
 
 def rss():
@@ -36,8 +39,29 @@ def rss():
             pass
 
 
-while True:
+count_end = 0
 
+
+def bot():
+    with app:
+        print(app.get_me())
+        print('1')
+        global count_end
+        count_find = collection.find().count()
+        all_news = collection.find()
+        print('count_find : ', count_find)
+        print('count_end : ', count_end)
+        if count_find > count_end:
+            for i in all_news:
+                print(i)
+                time.sleep(3)
+                app.send_message(-1001476077997, '{0} \n {1} \n {2}'.format(i['title'], i['summary'], i['published']))
+            count_end = count_find
+            print('count_end : ', count_end)
+
+
+while True:
     rss()
     print(datetime.datetime.now())
-    time.sleep(1)
+    bot()
+    time.sleep(10)
