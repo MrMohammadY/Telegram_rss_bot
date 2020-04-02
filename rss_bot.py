80643d255cafac6993d40e7b90378509732633ab
MMD <m.yazdani000@gmail.com>
2020-04-02 14:31:14 +0430

    adding deleted past news of mongodb and cleaning code

--- rss_bot.py
+++ rss_bot.py
@@ -1,67 +1,92 @@
 import datetime
 import feedparser
 import time
+import wget
+import os
 from pymongo import MongoClient
-from pyrogram import Client, MessageHandler
-
+from pyrogram import Client
 # ---------------------------------------- >> Connect to Mongodb
 client = MongoClient('localhost', 27017)
-db = client.test_database
-collection = db.mmd
+db = client.rss_bot
+collection = db.rss_news
 # ---------------------------------------- >> Connect to rss site
-feed = feedparser.parse("https://www.yjc.ir/fa/rss/3")
+feed = feedparser.parse("https://www.khabaronline.ir/rss/tp/1")
+
 app = Client('my_bot')
 
+chanel_id = -1001476077997
 
-def rss():
+
+def get_rss_news():
     # ---------------------------------------- >> Get information of rss site
-    feed_entries = feed.entries
-    for entry in feed_entries:
-        title = entry.title
-        summary = entry.summary
-        published = entry.published
-        img_link = entry.links
-        if img_link[1]['href']:
-            img_link = img_link[1]['href']
-        else:
-            pass
-        post = {'title': title, 'summary': summary, 'published': published, 'img_links': img_link}
-        # ---------------------------------------- >> Check information and insert to Mongodb
-        dd = datetime.date.today()
-        dd = dd.strftime('%d %b %Y')
-        if dd in published:
-            if collection.find(dict(title=post.get('title'))).count() != 0:
-                print('This value exists')
+    try:
+        feed_entries = feed.entries
+        for entry in feed_entries:
+            title = entry.title
+            summary = entry.summary
+            published = entry.published
+            img_link = entry.links
+            if img_link[1]['href']:
+                img_link = img_link[1]['href']
             else:
-                print('This value does not exist')
-                posts = collection.insert_one(post).inserted_id
+                pass
+            post = {'title': title, 'summary': summary, 'published': published, 'img_links': img_link}
+            # ---------------------------------------- >> Check information and insert to Mongodb
+            dd = datetime.date.today()
+            dd = dd.strftime('%d %b %Y')
+            if dd in published:
+                if collection.find(dict(title=post.get('title'))).count() != 0:
+                    print('This value exists')
+                else:
+                    print('This value does not exist')
+                    posts = collection.insert_one(post).inserted_id
+            else:
+                pass
+    except TimeoutError as Ter:
+        print('message Error : ', Ter)
+
+
+# --------------------------------------- >> Deleted past news of Mongodb
+def deleted_past_news():
+    dd = datetime.date.today()
+    dd = dd.strftime('%d %b %Y')
+    all_news = collection.find()
+    for i in all_news:
+        if dd in i['published']:
+            print('This news is new')
         else:
-            pass
+            collection.delete_one(i)
+            print('Message is deleted')
 
 
 count_end = 0
 
 
-def bot():
+def send_news():
     with app:
-        print(app.get_me())
-        print('1')
         global count_end
         count_find = collection.find().count()
-        all_news = collection.find()
-        print('count_find : ', count_find)
-        print('count_end : ', count_end)
-        if count_find > count_end:
-            for i in all_news:
-                print(i)
-                time.sleep(3)
-                app.send_message(-1001476077997, '{0} \n {1} \n {2}'.format(i['title'], i['summary'], i['published']))
-            count_end = count_find
-            print('count_end : ', count_end)
+        all_news = collection.find().sort('published')
+        try:
+            if count_find > count_end:
+                for i in all_news:
+                    if i['img_links']:
+                        img_file = wget.download(i['img_links'])
+                        time.sleep(2)
+                        app.send_photo(chanel_id, photo=img_file, caption='✉ {0} \n\n 🔎{1} \n\n ✅@khazar_online'.format(i['title'], i['summary']))
+                        print('Send message')
+                        os.remove(img_file)
+                    else:
+                        time.sleep(2)
+                        app.send_message(chanel_id, '✉ {0} \n\n 🔎{1} \n\n ✅@khazar_online'.format(i['title'], i['summary']))
+                deleted_past_news()
+                count_end = count_find
+        except TimeoutError as Ter:
+            print('message Error : ', Ter)
 
 
 while True:
-    rss()
+    get_rss_news()
     print(datetime.datetime.now())
-    bot()
-    time.sleep(10)
+    send_news()
+    time.sleep(120)
