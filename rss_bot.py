1509e1ae40a3889b5c99b68ccfb82ce359c30c14
MMD <m.yazdani000@gmail.com>
2020-04-04 22:49:26 +0430

    get news of multi site

--- rss_bot.py
+++ rss_bot.py
@@ -5,42 +5,61 @@ import wget
 import os
 from pymongo import MongoClient
 from pyrogram import Client
+from urllib.error import URLError
 # ---------------------------------------- >> Connect to Mongodb
 client = MongoClient('localhost', 27017)
 db = client.rss_bot
 collection = db.rss_news
-# ---------------------------------------- >> Connect to rss site
+# ---------------------------------------- >> Connect to Bot
 app = Client('my_bot')
 
+# ---------------------------------------- >> Chanel _id
 chanel_id = -1001476077997
 
+# ---------------------------------------- >> Get url site of File.txt
+with open('C:/Users/Mohammad/Desktop/site.txt', 'r') as site:
+    site = site.read()
+    site = site.split('\n')
+l_site = len(site)
 
+
+# ---------------------------------------- >> Get information of rss site
 def get_rss_news():
-    # ---------------------------------------- >> Get information of rss site
+    global l_site, published
     try:
-        feed = feedparser.parse("https://www.khabaronline.ir/rss/tp/1")
-        feed_entries = feed.entries
-        for entry in feed_entries:
-            title = entry.title
-            summary = entry.summary
-            published = entry.published
-            img_link = entry.links
-            if img_link[1]['href']:
-                img_link = img_link[1]['href']
-            else:
-                pass
-            post = {'title': title, 'summary': summary, 'published': published, 'img_links': img_link}
-            # ---------------------------------------- >> Check information and insert to Mongodb
-            dd = datetime.date.today()
-            dd = dd.strftime('%d %b %Y')
-            if dd in published:
-                if collection.find(dict(title=post.get('title'))).count() != 0:
-                    print('This value exists')
+        for i in range(0, l_site):
+            feed = feedparser.parse(site[i])
+            feed_entries = feed.entries
+            for entry in feed_entries:
+                title = entry.title
+
+                if entry.get('summary'):
+                    summary = entry.summary
                 else:
-                    print('This value does not exist')
-                    posts = collection.insert_one(post).inserted_id
-            else:
-                pass
+                    summary = None
+
+                if entry.published:
+                    published = entry.published
+                elif entry.pubDate:
+                    published = entry.pubDate
+
+                if len(entry.links) == 2:
+                    img_link = entry.links[1]['href']
+                else:
+                    img_link = None
+
+                post = {'title': title, 'summary': summary, 'published': published, 'img_links': img_link}
+                # ---------------------------------------- >> Check information and insert to Mongodb
+                dd = datetime.date.today()
+                dd = dd.strftime('%d %b %Y')
+                if dd in published:
+                    if collection.find(dict(title=post.get('title'))).count() != 0 or collection.find(dict(title=post.get('summary'))).count() != 0:
+                        print('This value exists')
+                    else:
+                        print('This value does not exist')
+                        posts = collection.insert_one(post).inserted_id
+                else:
+                    pass
     except TimeoutError as Ter:
         print('message Error : ', Ter)
 
@@ -61,22 +80,28 @@ def deleted_past_news():
 count_end = 0
 
 
+# ----------------------------------- >> Send news to chanel
 def send_news():
-    with app:
-        global count_end
-        all_news = list(collection.find().sort('published'))
-        count_all = len(all_news)
-        for i in all_news[count_end:count_all]:
-            if i['img_links']:
-                img_file = wget.download(i['img_links'])
-                app.send_photo(chanel_id, photo=img_file, caption='✉ {0} \n\n 🔎{1} \n\n '.format(i['title'], i['summary']))
-                os.remove(img_file)
-                print('This message :', i['title'], 'is SEND...')
-            else:
-                app.send_message(chanel_id, '✉ {0} \n\n 🔎{1} \n\n '.format(i['title'], i['summary']))
-        deleted_past_news()
-        count_end = count_all
-        print(count_end)
+    try:
+        with app:
+            global count_end
+            all_news = list(collection.find().sort('published'))
+            count_all = len(all_news)
+            for i in all_news[count_end:count_all]:
+                if i['img_links']:
+                    time.sleep(10)
+                    img_file = wget.download(i['img_links'])
+                    app.send_photo(chanel_id, photo=img_file, caption='✉ {0} \n\n 🔎{1} \n\n ✅@chanel_id'.format(i['title'], i['summary']))
+                    os.remove(img_file)
+                    print('This message :', i['title'], 'is SEND...')
+                else:
+                    time.sleep(10)
+                    app.send_message(chanel_id, '✉ {0} \n\n 🔎{1} \n\n ✅@chanel_id'.format(i['title'], i['summary']))
+            deleted_past_news()
+            count_end = count_all
+            print(count_end)
+    except URLError or TimeoutError:
+        pass
 
 
 while True:
